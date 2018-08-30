1e0dfd59d94dfea66dfa7179d0fe5c483d1fde83
gy <1576369090@qq.com>
2018-08-30 22:10:17 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('SITE_NAME', '灰灰鸽的页面');
+define('TITLENAME', 'HUIHUIGE');
 define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('EMAIL', 'guyanhui666@protonmail.com');
 ?>
