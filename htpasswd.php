e6180ec5969a0bf50ad745463720bea906b835d8
gy <1576369090@qq.com>
2018-08-31 19:52:05 +0800

    Add files via upload

---
+++ htpasswd.php
@@ -0,0 +1,10 @@
+<？PHP
+//要为.htpasswd文件加密的密码
+$ clearTextPassword ='0537ab...';
+
+//加密密码
+$ password = crypt（$ clearTextPassword，base64_encode（$ clearTextPassword））;
+
+//打印加密密码
+echo $ password;
+？>
