7a7dc9786d6e62478ba8658d76fb37e725b283be
gy <1576369090@qq.com>
2018-09-02 20:02:50 +0800

    Add files via upload

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
-<？PHP
-$ gl =（isset（$ _COOKIE [ ' gl ' ]）&&  $ _COOKIE [ ' gl ' ]）？$ _COOKIE [ ' gl ' ]：' HK ' ;
-define（' ROOT_PART '，Root_part（））;
-define（' APIKEY '，'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c '）;
-define（' GJ_CODE '，$ gl）;
-define（' SITE_NAME '，'灰灰鸽的页面'）;
-define（' TITLENAME '，'HUIHUIGE'）;
-define（' EN2DEKEY '，' 0090909i604wkkfkak '）;
-define（' EMAIL '，'guyanhui666@protonmail.com'）;
-？>
+<?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
+define('ROOT_PART', Root_part());
+define('APIKEY', '填写你的GOOGLE API KEY');
+define('GJ_CODE', $gl);
+define('SITE_NAME', '网站名称');
+define('TITLENAME', '标题名称');
+define('EN2DEKEY', '0090909i604wkkfkak');
+define('EMAIL', '你的邮箱');
+?>
