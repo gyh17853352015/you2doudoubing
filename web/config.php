829b2da8459dee0e43f0716bfacef56a7f499d36
gy <1576369090@qq.com>
2018-09-02 20:04:51 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
 define('ROOT_PART', Root_part());
-define('APIKEY', '填写你的GOOGLE API KEY');
+define('APIKEY', 'AIzaSyAc7TlHTmooaxXEQ64u0k9LB4uZixskXD0');
 define('GJ_CODE', $gl);
-define('SITE_NAME', '网站名称');
-define('TITLENAME', '标题名称');
+define('SITE_NAME', '灰灰鸽');
+define('TITLENAME', 'HUIHUIGE');
 define('EN2DEKEY', '0090909i604wkkfkak');
-define('EMAIL', '你的邮箱');
+define('EMAIL', 'guyanhui666@gmail.com');
 ?>
