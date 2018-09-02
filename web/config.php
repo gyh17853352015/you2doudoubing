3f337509e15e08c5823086ac74d3646ac9f5f7d3
gy <1576369090@qq.com>
2018-09-02 19:54:41 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,10 @@
-<?php
-define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
-define('GJ_CODE', 'JP');
-define('SITE_NAME', '灰灰鸽的页面');
-define('TITLENAME', 'HUIHUIGE');
-define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'guyanhui666@protonmail.com');
-?>
+<？PHP
+$ gl =（isset（$ _COOKIE [ ' gl ' ]）&&  $ _COOKIE [ ' gl ' ]）？$ _COOKIE [ ' gl ' ]：' HK ' ;
+define（' ROOT_PART '，Root_part（））;
+define（' APIKEY '，'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c '）;
+define（' GJ_CODE '，$ gl）;
+define（' SITE_NAME '，'灰灰鸽的页面'）;
+define（' TITLENAME '，'HUIHUIGE'）;
+define（' EN2DEKEY '，' 0090909i604wkkfkak '）;
+define（' EMAIL '，'guyanhui666@protonmail.com'）;
+？>
